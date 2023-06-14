12579a042e3ab5a855aa184f7da4f35ceaa34783
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-06-14 18:06:13 +0200

    milestone 3

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,72 +1,65 @@
 <template>
-    <div class="body">
+    <Loader v-if="projects.length === 0"/>
+    <div class="body overflow-auto">
   
-      <div class="container">
-        <h1 class="text-light">{{ title }}</h1>
-        <div class="row">
-          <div class="col-4" v-for="(project, index) in projects" :key="project.id">
-  
-            <div class="card text-bg-dark mb-3 shadow">
-              <div class="card-header m-auto">
-                <img :src="project.image" :alt="project.title" class="img-fluid">
-              </div>
-  
-              <div class="card-body">
-                <h4 class="fs-4 card-title fw-bold text-center text-capitalize py-2">{{ project.title }}</h4>
-                <p class="card-text">{{ project.title }}</p>
-              </div>
+        <div class="container">
+            <h1 class="text-light">{{ title }}</h1>
+            <div class="row">
+                <ProjectCard v-for="(project, index) in projects" :key="project.id" :project="project"/>
             </div>
-            
-          </div>
+    
+            <nav aria-label="Page navigation example">
+            <ul class="pagination">
+            <li class="page-item"><button :class="{ 'page-link': true, 'disabled': currentPage === 1 }" @click="getData(currentPage - 1)">Previous</button></li>
+            <li class="page-item" v-for="n in lastPage"><button :class="{ 'page-link': true, 'active': currentPage === n }" @click="getData(n)">{{ n }}</button></li>
+            <li class="page-item"><button :class="{ 'page-link': true, 'disabled': currentPage === 4 }" @click="getData(currentPage + 1)">Next</button></li>
+            </ul>
+        </nav>
+    
         </div>
-  
-        <nav aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item"><button :class="{ 'page-link': true, 'disabled': currentPage === 1 }" @click="getData(currentPage - 1)">Previous</button></li>
-          <li class="page-item" v-for="n in lastPage"><button :class="{ 'page-link': true, 'active': currentPage === n }" @click="getData(n)">{{ n }}</button></li>
-          <li class="page-item"><button :class="{ 'page-link': true, 'disabled': currentPage === 4 }" @click="getData(currentPage + 1)">Next</button></li>
-        </ul>
-      </nav>
-  
-      </div>
     </div>
   </template>
   
-  <script>
-  import axios from 'axios';
+<script>
+    import axios from 'axios';
+    import Loader from '../components/Loader.vue';
+    import ProjectCard from '../components/ProjectCard.vue';
+    export default {
+        name: 'App',
+        components: {
+            Loader,
+            ProjectCard,
+        },
+        data() {
+            return {
+                title: 'Project list',
+                projects: [],
+                apiUrl: 'http://127.0.0.1:8000/api',
+                currentPage: 1,
+                lastPage: null,
+            }
+        },
+        methods: {
+            getData(numPage) {
+                axios.get(`${this.apiUrl}/projects`, {
+                    params: {
+                        'page': numPage
+                    }
+                }).then((res) => {
+                    //console.log(res);
+                    this.projects = res.data.results.data;
+                    this.currentPage = res.data.results.current_page;
+                    this.lastPage = res.data.results.last_page;
+                })
+            }
+        },
+        mounted() {
+            this.getData(1);
+        }
+    
+    }
+</script>
   
-  export default {
-      'name': 'App',
-      data() {
-          return {
-              title: 'Project list',
-              projects: [],
-              apiUrl: 'http://127.0.0.1:8000/api',
-              currentPage: 1,
-              lastPage: null,
-          }
-      },
-      methods: {
-          getData(numPage) {
-              axios.get(`${this.apiUrl}/projects`, {
-                  params: {
-                      'page': numPage
-                  }
-              }).then((res) => {
-                  //console.log(res);
-                  this.projects = res.data.results.data;
-                  this.currentPage = res.data.results.current_page;
-                  this.lastPage = res.data.results.last_page;
-              })
-          }
-      },
-      mounted() {
-          this.getData(1);
-      }
-  
-  }
-  </script>
-  
-  <style scoped>
-  
-  </style>
+<style scoped>
+
+</style>
--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,11 +1,47 @@
 <template>
-    <h1> Single product </h1>
+    <Loader v-if="!project"/>
+    <div v-if="project">
+		<h1> {{project.title}} </h1>
+        <img class="img-fluid" :src="project.image" :alt="project.title">
+		<!-- <img :src="imgBasePath + project.cover_image" :alt="project.title">
+		<ul>
+			<li v-for="color in project.colors" :style="{backgroundColor: color.hex_value}"> {{color.name}}</li>
+		</ul> -->
+	</div>
 </template>
 
 <script>
+import axios from 'axios';
+import Loader from '../components/Loader.vue';
 export default  {
     name: 'SingleProject',
-    
+    components: {
+        Loader,
+    },
+    data(){
+        return{
+            project: null,
+            apiUrl: 'http://localhost:8000/api',
+        }
+    },
+    methods:{
+        getProjects(){
+            axios.get(`${this.apiUrl}/projects/${this.$route.params.slug}`).then((res)=>{
+                //console.log(res.data.results);
+                if(res.data.success){
+                    this.project = res.data.results;
+                    console.log(this.project);
+                } else{
+                    this.$router.push({name : 'not found'})
+                }
+            })
+        }
+    },
+    mounted(){
+        //console.log(this.$router);
+        //console.log(this.$route);
+        this.getProjects();
+    }
 }
 </script>
 
